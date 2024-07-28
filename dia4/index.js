e73e7951846aab560257171bd8ec28a9526c2755
Wellyngthon Sanveira <wellyngthonsanveira@gmail.com>
2024-07-28 17:40:36 -0300

    dia4

--- dia4/index.js
+++ dia4/index.js
@@ -1,6 +1,11 @@
-let condicao = true;
-if (condicao) {
-  console.log("entrou no if");
+let fome = prompt("Você está com fome? s ou n?");
+let dinheiro = prompt("Você tem dinheiro? s ou s?");
+let restaurante = prompt("O restaurante ta aberto? s ou n?");
+
+if (fome == "n" || dinheiro == "n") {
+  console.log("Hoje a janta será em casa");
+} else if (fome == "s" && dinheiro == "s" && restaurante == "n") {
+  console.log("Peça um Delivery");
 } else {
-  console.log("entrou no else");
+  console.log("Hoje o jantar será no seu restaurante preferido!");
 }
