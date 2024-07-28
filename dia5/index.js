7b006037fe97f350049a4df8d270751723fb3803
Wellyngthon Sanveira <wellyngthonsanveira@gmail.com>
2024-07-28 18:38:06 -0300

    dia5

---
+++ dia5/index.js
@@ -0,0 +1,13 @@
+let idade = Number(prompt("Qual sua Idade?"));
+
+switch (idade) {
+  case 10:
+    console.log("Você tem 10 anos");
+    break;
+  case 15:
+    console.log("Você tem 15 anos");
+    break;
+  default:
+    console.log("Você tem mais de 15 anos");
+    break;
+}
