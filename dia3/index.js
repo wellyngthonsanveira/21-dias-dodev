e15b10e9039e84b7d73e67cdce4c1500345b84d4
Wellyngthon Sanveira <wellyngthonsanveira@gmail.com>
2024-07-27 23:39:26 -0300

    Dia 3

---
+++ dia3/index.js
@@ -0,0 +1,10 @@
+let nome = "JOAO";
+let idade = 22;
+let altura = 1.83;
+let peso = 75;
+
+let nascimento = 2024 - idade;
+console.log("você nasceu em " + nascimento);
+
+let imc = peso / (altura * altura);
+console.log("seu imc é: " + imc);
