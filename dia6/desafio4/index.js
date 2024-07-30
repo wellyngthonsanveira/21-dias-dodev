07920ed5c06d648b6f71de254e11a77510dcf1c4
Wellyngthon Sanveira <wellyngthonsanveira@gmail.com>
2024-07-29 21:48:26 -0300

    dia6 desafio4

---
+++ dia6/desafio4/index.js
@@ -0,0 +1,13 @@
+let numeroTabuada = Number(prompt("Digite o numero para saber a tabuada"));
+
+for (let i = 0; i <= 10; i++) {
+  console.log(numeroTabuada + " * " + i + " = " + numeroTabuada * i);
+}
+numeroTabuada += 1;
+for (let i = 0; i <= 10; i++) {
+  console.log(numeroTabuada + " * " + i + " = " + numeroTabuada * i);
+}
+numeroTabuada += 1;
+for (let i = 0; i <= 10; i++) {
+  console.log(numeroTabuada + " * " + i + " = " + numeroTabuada * i);
+}
